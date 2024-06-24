77c8679ffa24a122e1770193b9d9e0987557a8c4
NathAmd <nathanaelamd@gmail.com>
2024-06-24 16:48:16 +0200

    update login/register + password hashed

--- assets/php/register.php
+++ assets/php/register.php
@@ -6,9 +6,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>pokedex</title>
     <link rel="stylesheet" href="../css/style.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 </head>
 
 <body>
+    <?php include_once("./header.php"); ?>
 
     <?php
     session_start();
@@ -29,12 +31,33 @@
 
         <form method="post" action="<?php echo $_SERVER['PHP_SELF'] ?>">
             <label>Username: </label><input type="text" name="username" class="inputfield"><br>
-            <label>Password: </label><input type="password" name="password" class="inputfield"><br>
-            <label>Repeat: </label><input type="password" name="repassword" class="inputfield"><br>
+            <label>Password: </label><input type="password" name="password" class="inputfield" id="password"><br>
+            <label>Repeat: </label><input type="password" name="repassword" class="inputfield" id="repassword"><br>
+            <label id="error-repassword"></label><br>
             <label>Email: </label><input type="text" name="email" class="inputfield"><br>
             <input type="submit" value="inscription" class="button"><br>
             <p><a href="./login.php">login</a></p>
         </form>
+
+        <script>
+            $("#repassword").on('input', function(e) {
+                console.log($("#repassword").val())
+                if ($("#repassword").val() != $("#password").val()) {
+                    $("#error-repassword").text("password does not match")
+                } else {
+                    $("#error-repassword").text("")
+                }
+            })
+
+            $(".button").on('mouseenter touchstart', function(e) {
+                e.target.type = "button"
+                if ($("#password").val().length > 3 && $("input[name=username]").val().length > 3) {
+                    if ($("#repassword").val() == $("#password").val()) {
+                        e.target.type = "submit"
+                    }
+                }
+            })
+        </script>
 
     <?php
         echo "</main>";
@@ -62,20 +85,28 @@
             echo "Username already exists";
         } else {
             $stmt = $conn->prepare("INSERT INTO users (username, password, email, admin, favoris) VALUES (:username, :password, :email, :admin, :favoris)");
-            $stmt->bindParam(':username', $_POST["username"]);
-            $stmt->bindParam(':password', $_POST["password"]);
-            $stmt->bindParam(':email', $_POST["email"]);
+            $stmt->bindParam(':username', $username);
+            $stmt->bindParam(':password', $hashed_password);
+            $stmt->bindParam(':email', $email);
             $stmt->bindParam(':admin', $admin);
             $stmt->bindParam(':favoris', $fav);
 
             $admin = 0;
             $fav = '{"fav":[]}';
 
+            $username = htmlspecialchars(trim(filter_input(INPUT_POST, "username", FILTER_SANITIZE_FULL_SPECIAL_CHARS)));
+            $password = htmlspecialchars(trim(filter_input(INPUT_POST, "password", FILTER_SANITIZE_FULL_SPECIAL_CHARS)));
+            $email = filter_var($_POST["email"], FILTER_VALIDATE_EMAIL);
 
-            $stmt->execute();
+            $hashed_password = password_hash($password, PASSWORD_DEFAULT);
 
-            header('Location: login.php'); // Redirect to login.php
-            exit;
+            if (strlen($username) > 3 && strlen($password) > 3) {
+                $stmt->execute();
+                header('Location: login.php'); // Redirect to login.php
+                exit;
+            } else {
+                echo "form not valid";
+            }
         }
     }
 
