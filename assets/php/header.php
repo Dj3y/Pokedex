564fd2783792d2fa893ab6c610d1a798cb6a8ecc
Stacy Druart <stacy@StacyDruartMSI>
2024-06-24 14:17:43 +0200

    maj

--- assets/php/header.php
+++ assets/php/header.php
@@ -1,12 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Pokédex</title>
-    <link rel="stylesheet" href="assets/css/style.css">
-</head>
-<body>
+<header>
     <img class="logo" src="assets/img/pokemon_logo.png" alt="logo pokemon">
     <!-- source du logo https://access.pokemon.com/images/pokemon_logo.png -->
     <nav>
@@ -14,3 +6,4 @@
         <a href="assets/php/index.php">My account</a>
         <a href="assets/php/register.php">Register</a>
     </nav>
+</header>
